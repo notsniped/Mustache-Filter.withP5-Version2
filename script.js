685b0e41454c3229bdb11bcb39be9f21ef4d23e7
SniperBlaze <72265661+shoddyDEV@users.noreply.github.com>
2021-08-29 12:27:35 +0530

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,8 @@ function preload() {
 }
 function setup() {
     canvas = createCanvas(300, 300);
-    canvas.position(660, 150);
+    canvas.position(300, 150);
+    canvas.center();
     video = createCapture(VIDEO);
     video.size = (300, 300);
     video.hide();
